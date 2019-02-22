a3db7ea3395d94462ec3515d090d9d8a791525eb
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2019-02-21 20:34:32 -0600

    Finished Slideshow
    Also adding the information for the 2nd card

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,3 +7,30 @@ function main() {
 function loadStuff() {
 	$("#top-nav").load("/docs/pages/top_nav.html")
 }
+
+function nextSlide(obj, amount) {
+	var slides = $(obj).parent().children(".slide");
+	var numOfSlides = slides.length;
+	var visibleIndex = 0;
+
+	indexSearcher:
+	for (var i = 0; i < numOfSlides; i++) {
+		if ($(slides[i]).hasClass("visible")) {
+			$(slides[i]).addClass("hidden");
+			$(slides[i]).removeClass("visible");
+			visibleIndex = i;
+			break indexSearcher;
+		}
+	}
+
+	if (amount == -1 && visibleIndex == 0) {
+		visibleIndex = numOfSlides - 1;
+	} else if (amount == 1 && visibleIndex == numOfSlides - 1) {
+		visibleIndex = 0
+	} else {
+		visibleIndex += amount;
+	}
+
+	$(slides[visibleIndex]).removeClass("hidden");
+	$(slides[visibleIndex]).addClass("visible");
+}
